b5d138d50236b8e8a01ad11dd1e8e0ad9c3cd8fc
尼克杨 <nickyang@NickPro.local>
2017-06-10 23:44:15 +0800

    [Feature] Add regiester layout

--- app/src/main/java/com/monkeylive/monkeyordering/MainActivity.java
+++ app/src/main/java/com/monkeylive/monkeyordering/MainActivity.java
@@ -2,12 +2,21 @@ package com.monkeylive.monkeyordering;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+        Button buttonLogin = (Button)findViewById(R.id.buttonGoToReg);
+        buttonLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_reg);
+            }
+        });
     }
 }
