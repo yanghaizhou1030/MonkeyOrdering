e57c8acacef06e111c09bdeb8b463676915e1ff5
尼克杨 <nickyang@NickPro.local>
2017-06-11 23:06:24 +0800

    [Feature] Use customize titleBar in activity_login

--- app/src/main/java/com/monkeylive/monkeyordering/MainActivity.java
+++ app/src/main/java/com/monkeylive/monkeyordering/MainActivity.java
@@ -1,11 +1,14 @@
 package com.monkeylive.monkeyordering;
 
+import android.app.Activity;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+//public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
